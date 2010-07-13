1c9268412db607f03adcca0770a3302c44e8b0f8
Steffen Mueller <smueller@cpan.org>
2010-07-13 11:28:51 +0200

    cleanup

--- xt/thread_0mq.pl
+++ xt/thread_0mq.pl
@@ -5,15 +5,13 @@ use Config;
 use ZeroMQ qw/:all/;
 
 BEGIN {
-    if ( $Config{useithreads} ) {
-
-        # We have threads
-        require threads;
-
-    }
-    else {
-        die 'Need threads to test ';
-    }
+  if ( $Config{useithreads} ) {
+    # We have threads
+    require threads;
+  }
+  else {
+    die 'Need threads to test ';
+  }
 }
 
 if (@ARGV != 3) {
@@ -29,46 +27,45 @@ my $roundtrip_count = shift @ARGV;
 my $local_thr = threads->create( \&local );
 
 sub local {
-    my $cxt = ZeroMQ::Context->new(1);
-    my $sock = ZeroMQ::Socket->new( $cxt, ZMQ_REP );
-	print "[local] Trying to start at $addr \n";
+  my $cxt = ZeroMQ::Context->new(1);
+  my $sock = ZeroMQ::Socket->new( $cxt, ZMQ_REP );
+  print "[local] Trying to start at $addr \n";
 
-    $sock->bind($addr);
-    my $msg;
-    foreach ( 1 .. $roundtrip_count ) {
-
-        #warn "$_\n" if (not $_ % 1000);
-        $msg = $sock->recv();
-        die "Bad size" if $msg->size() != $msg_size;
-        $sock->send($msg);
-    }
+  $sock->bind($addr);
+  my $msg;
+  foreach ( 1 .. $roundtrip_count ) {
+    #warn "$_\n" if (not $_ % 1000);
+    $msg = $sock->recv();
+    die "Bad size" if $msg->size() != $msg_size;
+    $sock->send($msg);
+  }
 }
 
 my $remote_thr = threads->create( \&remote );
 
 sub remote {
-    my $cxt = ZeroMQ::Context->new(1);
-    my $sock = ZeroMQ::Socket->new( $cxt, ZMQ_REQ );
-	print "[remote]Trying to start at $addr \n";
+  my $cxt = ZeroMQ::Context->new(1);
+  my $sock = ZeroMQ::Socket->new( $cxt, ZMQ_REQ );
+  print "[remote]Trying to start at $addr \n";
 
-    $sock->connect($addr);
-    my $text = '0' x $msg_size;
-    my $msg  = ZeroMQ::Message->new($text);
+  sleep 1;
+  $sock->connect($addr);
+  my $text = '0' x $msg_size;
+  my $msg  = ZeroMQ::Message->new($text);
 
-    my $before = time();
-    foreach ( 1 .. $roundtrip_count ) {
+  my $before = time();
+  foreach ( 1 .. $roundtrip_count ) {
 
-        #warn "$_\n" if (not $_ % 1000);
-        $sock->send($msg);
-        $msg = $sock->recv();
-        die "Bad size" if $msg->size() != $msg_size;
-    }
-    my $after = time();
-    my $latency = ( $after - $before ) / ( $roundtrip_count * 2 ) * 1.e6;
+    #warn "$_\n" if (not $_ % 1000);
+    $sock->send($msg);
+    $msg = $sock->recv();
+    die "Bad size" if $msg->size() != $msg_size;
+  }
+  my $after = time();
+  my $latency = ( $after - $before ) / ( $roundtrip_count * 2 ) * 1.e6;
 }
 
 END {
-
-    $local_thr->join();
-    $remote_thr->join();
+  $local_thr->join() if defined $local_thr;
+  $remote_thr->join() if defined $remote_thr;
 }
