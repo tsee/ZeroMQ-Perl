c8046d9c73dd8f8c509b163736d5e8deb81ebd17
lestrrat <lestrrat+github@gmail.com>
2010-08-18 15:35:59 +0900

    add hello work example

---
+++ eg/hello_client.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+use strict;
+use ZeroMQ qw(ZMQ_REQ);;
+
+my ($host, $port);
+
+if (@ARGV >= 2) {
+    ($host, $port) = @ARGV;
+} elsif (@ARGV) {
+    if ($ARGV[0] =~ /^([\w\.]+):(\d+)$/) {
+        ($host, $port) = ($1, $2);
+    } else {
+        $host = $ARGV[0];
+    }
+}
+$host ||= '127.0.0.1';
+$port ||= 5566;
+
+my $ctxt = ZeroMQ::Context->new();
+my $sock = $ctxt->socket(ZMQ_REQ);
+$sock->connect( "tcp://$host:$port" );
+$sock->send("hello");
+
+my $message = $sock->recv();
+
+print $message->data, "\n";
