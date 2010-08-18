ef60eac08aaf0a615c522a1cebcb2539c7836495
lestrrat <lestrrat+github@gmail.com>
2010-08-18 15:49:22 +0900

    add pubsub example

---
+++ eg/pubsub_client.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+use strict;
+use ZeroMQ qw(ZMQ_SUB ZMQ_SUBSCRIBE);
+
+my ($host, $port);
+
+if (@ARGV >= 2) {
+    ($host, $port) = @ARGV;
+} elsif (@ARGV) {
+    if ($ARGV[0] =~ /^([\w\.]+):(\d+)$/) {
+        ($host, $port) = ($1, $2);
+    } else {
+        $host = $ARGV[0];
+    }
+}
+$host ||= '127.0.0.1';
+$port ||= 5566;
+
+my $ctxt = ZeroMQ::Context->new();
+my $sock = $ctxt->socket(ZMQ_SUB);
+$sock->connect( "tcp://$host:$port" );
+$sock->setsockopt(ZMQ_SUBSCRIBE, "W");
+
+while (1) {
+    my $message = $sock->recv();
+    print $message->data, "\n";
+}
---
+++ eg/pubsub_server.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+use strict;
+use ZeroMQ qw(ZMQ_PUB);
+
+my ($host, $port);
+
+if (@ARGV >= 2) {
+    ($host, $port) = @ARGV;
+} elsif (@ARGV) {
+    if ($ARGV[0] =~ /^([\w\.]+):(\d+)$/) {
+        ($host, $port) = ($1, $2);
+    } else {
+        $host = $ARGV[0];
+    }
+}
+$host ||= '127.0.0.1';
+$port ||= 5566;
+
+my $ctxt = ZeroMQ::Context->new();
+my $sock = $ctxt->socket(ZMQ_PUB);
+$sock->bind( "tcp://$host:$port" );
+
+while (1) {
+    $sock->send("HELLO?");
+    $sock->send("WORLD?");
+    sleep 2;
+}
