3a8e0d34d9377efde2e014092129b980438f3504
lestrrat <lestrrat+github@gmail.com>
2010-08-20 21:05:05 +0900

    git head introduces ZMQ_PULL / ZMQ_PUSH, but the version currently stays the same, so we just fake it

--- xs/perl_zeromq.h
+++ xs/perl_zeromq.h
@@ -13,4 +13,16 @@ typedef struct {
 typedef void PerlZMQ_Socket;
 typedef zmq_msg_t PerlZMQ_Message;
 
+/* ZMQ_PULL was introduced for version 3, but it exists in git head.
+ * it's just rename of ZMQ_UPSTREAM and ZMQ_DOWNSTREAM so we just
+ * fake it here
+ */
+#ifndef ZMQ_PULL
+#define ZMQ_PULL ZMQ_UPSTREAM
+#endif
+
+#ifndef ZMQ_PUSH
+#define ZMQ_PUSH ZMQ_DOWNSTREAM
+#endif
+
 #endif /* __PERL_ZERMQ_H__ */
