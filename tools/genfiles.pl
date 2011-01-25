c27968f02db98409a5c085d968ac199cf252b5d0
lestrrat <lestrrat+github@gmail.com>
2011-01-25 14:03:13 +0900

    apply a modified version of https://github.com/lestrrat/ZeroMQ-Perl/pull/5

--- tools/genfiles.pl
+++ tools/genfiles.pl
@@ -1,5 +1,7 @@
 use strict;
+use Config;
 use File::Spec;
+use File::Basename ();
 
 write_constants_file( File::Spec->catfile('xs', 'const-xs.inc') );
 write_typemap( File::Spec->catfile('xs', 'typemap') );
@@ -74,7 +76,22 @@ EOM
 sub write_constants_file {
     my $file = shift;
 
-    my $header = $ENV{ZMQ_H} || '/usr/local/include/zmq.h';
+    my $header = $ENV{ZMQ_H};
+    if ( ! $header ) {
+        my @locations = ('/usr/local', '/usr', File::Basename::dirname($^X) );
+        foreach my $loc ( @locations ) {
+            my $candidate = File::Spec->catfile( $loc, 'include', 'zmq.h' );
+            if ( -f $candidate ) {
+                $header = $candidate;
+                last;
+            }
+        }
+    }
+
+    if (! $header) {
+        die "Could not find zmq.h anywhere.";
+    }
+    print STDERR " + Using zmq.h from $header\n";
 
     open( my $in, '<', $header ) or
         die "Could not open file $header for reading: $!";
